8ebe86e361bbc3847caaee332eec38c960ff35c3
cabled510 <cafcem234@gmail.com>
2026-07-31 21:59:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ st.title(' 🤖 Machine Learning App')
 
 st.write('This is machine learning app')
 
+df  = pd.read_csv('https://raw.githubusercontent.com/cabled510/RICE-GERMPLASM-CLASSIFICATION-AND-PREDICTION-APP/refs/heads/master/RiceML/Preprocessed_Rice_Data.csv')
+df
+
