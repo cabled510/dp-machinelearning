7013f4912e0c5c913cd8be623039e8d34d7e9fa0
cabled510 <cafcem234@gmail.com>
2026-07-31 23:33:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ Y = df.Accession
 Y
 
 st.write('**Model Results**')
-Z = pd.read_csv('https://github.com/cabled510/RICE-GERMPLASM-CLASSIFICATION-AND-PREDICTION-APP/raw/refs/heads/master/RiceML/Model_Training_Results.csv')
-Z
+df = pd.read_csv('https://github.com/cabled510/RICE-GERMPLASM-CLASSIFICATION-AND-PREDICTION-APP/raw/refs/heads/master/RiceML/Model_Training_Results.csv')
+df
 
 
 
